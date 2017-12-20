f86141ac43930d28d07b2c29b7e9c28cd0b0559e
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-20 23:46:49 +0000

    Re-arranged init component

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+
+const Style = {
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#FF5A5F',
+  },
+};
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      Hello!
-    </p>
+  <div style={Style.container}>
+    Welcome to Scrumdiddly
   </div>
 );
 
