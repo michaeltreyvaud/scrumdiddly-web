08087bfbdb459c13881ebcbc3293cbc2526aedd8
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-12-21 17:02:40 +0000

    Updated login styles

--- src/App/Routes/Auth/Login.js
+++ src/App/Routes/Auth/Login.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppTheme from '../../../Themes';
 
-const Style = {
+const Styles = {
   container: {
     display: 'flex',
     flex: 1,
@@ -12,13 +12,69 @@ const Style = {
     color: AppTheme.white,
     fontWeight: 'bold',
     fontSize: AppTheme.largeFont,
+    flexDirection: 'column',
+  },
+  loginForm: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  loginFormEmailInput: {
+    color: AppTheme.white,
+    backgroundColor: 'transparent',
+    border: 'none',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '1px',
+    borderBottomColor: AppTheme.white,
+    fontSize: AppTheme.largeFont,
+    outline: 'none',
+  },
+  loginFormSubmitButton: {
+    height: '40px',
+    backgroundColor: AppTheme.white,
+    color: AppTheme.blue,
+    border: 'none',
+    borderRadius: '5px',
+    marginTop: '10px',
+    fontSize: AppTheme.mediumFont,
+    textTransform: 'uppercase',
+    outline: 'none',
   },
 };
 
-const Login = () => (
-  <div style={Style.container}>
-    Login
-  </div>
-);
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailAddress: '',
+    };
+  }
+  submitLoginForm(event) {
+    event.preventDefault();
+    console.log('Login Submitted');
+  }
+  render() {
+    return (
+      <div style={Styles.container}>
+        Login
+        <form
+          onSubmit={event => this.submitLoginForm(event)}
+          style={Styles.loginForm}
+        >
+          <input
+            placeholder="Email"
+            type="text"
+            style={Styles.loginFormEmailInput}
+            value={this.state.emailAddress}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            style={Styles.loginFormSubmitButton}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
